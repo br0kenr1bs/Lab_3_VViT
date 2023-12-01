4e080517d16344c3bfacddf442cf9862b8babe58
Матвей Надводнюк <145839983+br0kenr1bs@users.noreply.github.com>
2023-12-01 10:21:53 +0300

    Add files via upload

---
+++ lab3/main.js
@@ -0,0 +1,8 @@
+function togglePortfolio() {
+  var portfolioMenu = document.getElementById("portfolioMenu");
+  if (portfolioMenu.style.display === "none") {
+    portfolioMenu.style.display = "block";
+  } else {
+    portfolioMenu.style.display = "none";
+  }
+}
